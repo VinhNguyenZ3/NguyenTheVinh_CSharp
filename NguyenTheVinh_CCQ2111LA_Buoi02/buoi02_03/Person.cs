ad597715258a63dbf6edced23256c461c7b97689
VinhNguyenZ3 <131794544+VinhNguyenZ3@users.noreply.github.com>
2023-09-27 11:09:35 +0700

    Add files via upload

---
+++ NguyenTheVinh_CCQ2111LA_Buoi02/buoi02_03/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi02_03
+{
+    internal class Person
+    {
+        public string HoTen { get; set; }
+    }
+}
